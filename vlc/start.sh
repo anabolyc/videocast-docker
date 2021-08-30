6436d7f44c0aff1a4849b4a61de7842ef48970d5
andrey.malyshenko <andrey.malyshenko@gmail.com>
2021-08-30 13:40:40 +0000

    Added vlc part

--- vlc/start.sh
+++ vlc/start.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-CONFIG=./icecast.xml
-
-sed \
-    -e 's?<sources>2</sources>?<sources>'"$ICECAST_SRC_LIMIT"'</sources>?g' \
-    -e 's?<source-password>hackme</source-password>?<source-password>'"$ICECAST_PASSWORD"'</source-password>?g' \
-    -e 's?<relay-password>hackme</relay-password>?<relay-password>'"$ICECAST_PASSWORD"'</relay-password>?g' \
-    -e 's?<admin-password>hackme</admin-password>?<admin-password>'"$ICECAST_ADMPASSWORD"'</admin-password>?g' \
-    -e 's?<hostname>localhost</hostname>?<hostname>'"$ICECAST_HOST"'</hostname>?g' \
-    $ICECAST_CONFIG > $CONFIG
-
-cat $CONFIG
-
-/usr/bin/icecast2 -c $CONFIG
+# generate playlist
+echo "Generating playlist..."
+find "$ROOT_FOLDER" -type f | sed -e 's/^/setup channel1 input "/' -e 's/$/"/' > ./playlist.conf && \
+cat ./playlist.conf && \
+echo "Starting vls stream..." && \
+vlc -Ihttp --vlm-conf /app/vlm.conf --random --loop --network-caching 1000 --sout-mux-caching 2000 --clock-jitter 0
